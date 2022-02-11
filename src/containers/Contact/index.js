75d8de7dafea3883eeb860075e61f603ead0e43f
GABY CUEVA <septiemepersixth@gmail.com>
2022-02-11 16:33:35 -0700

    ajuste estilos

--- src/containers/Contact/index.js
+++ src/containers/Contact/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BigTitle, ContactInfo, Div, Flex, Line, Logo, Subtitle} from "./styles";
 import jakaBlack from "../../images/jaka-black.svg";
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 function Contact() {
     return (
@@ -14,7 +15,7 @@ function Contact() {
                     <Line />
                     <ContactInfo>
                         <span>contact@jaka.com</span>
-                        <span>+52 1 685 432 2053</span>
+                        <span><WhatsAppIcon /> +52 1 685 432 2053</span>
                     </ContactInfo>
                 </Flex>
             </Div>

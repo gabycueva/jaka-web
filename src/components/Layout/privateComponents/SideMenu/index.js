c4d3e0c1d35e933cb6be2e21e96becf158c74509
GABY CUEVA <septiemepersixth@gmail.com>
2022-02-23 22:30:21 -0700

    estilos menu

--- src/components/Layout/privateComponents/SideMenu/index.js
+++ src/components/Layout/privateComponents/SideMenu/index.js
@@ -1,16 +1,58 @@
-import React from 'react';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
 import MenuIcon from '@mui/icons-material/Menu';
-import {Div} from "./styles";
-
+import { Div, List } from './styles';
+import { Link } from "react-router-dom";
 
 function SideMenu() {
-    return (
-        <Div>
-            <div className="hamburguer">
-                <MenuIcon/>
-            </div>
-        </Div>
-    );
+  const [state, setState] = React.useState({
+    top: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+      <ul>
+                <li><Link to="/portfolio">Portfolio</Link></li>
+                <li><Link to="/collection">Collection</Link></li>
+                <li><Link to="/bio">Bio</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+      </ul>
+      </List>
+    </Box>
+  );
+
+  return (
+    <Div>
+      {['top'].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}><div className='hamburguer'><MenuIcon /></div></Button>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </Div>
+  );
 }
 
 export default SideMenu;
